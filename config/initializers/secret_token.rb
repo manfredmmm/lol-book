aecc4c32b5cb722b0ce35a93b6b87b4f4c10e913
manfred <mkmanfred@gmail.com>
2010-12-01 17:59:39 +0100

    rails installat

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LolBook::Application.config.secret_token = '16bfd5d6879dbecfea7e85de084bd305491c529980474b24886cd705e3ae51b098ac5667e487db71ef3705a5fc1ae40ca4849b075fdb70c64b1b841bae6b1ea8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+LolBook::Application.config.session_store :cookie_store, :key => '_lol-book_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# LolBook::Application.config.session_store :active_record_store
